f436cfaa1b802bef593bad05ad8efb89307f2ab1
cam-br0wn <brownc2@carleton.edu>
2021-11-19 20:46:27 -0600

    fixed class mbit serial read script

--- class_mbit_read.sh
+++ class_mbit_read.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-python3 -m serial.tools.miniterm --raw --eol CR --encoding ascii /dev/cu.usbmodem0007820063751 38400
+python3 -m serial.tools.miniterm --raw --eol CR --encoding ascii /dev/cu.usbmodem0000001234561 38400
 
